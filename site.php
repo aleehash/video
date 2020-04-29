59a6010601786941589eb281567c2cd4d0787340
aleehash <64203943+aleehash@users.noreply.github.com>
2020-04-30 01:11:35 +0500

    Add files via upload

---
+++ site.php
@@ -0,0 +1,19 @@
+<!DOCCTYPE html>
+<html>
+    <head>
+     <meta charset="utf-8">
+    
+    </head>
+<body>
+
+<video controls width="640" height="380" controls >
+  <source src="aaa.mp4" type="video/mp4">
+  
+<div style="border: 1px solid black ; padding: 8px ;">
+Sorry, your browser does not support the &lt;video&gt; tag used in this demo.
+</div>
+</video>
+
+</body>
+
+</html>
